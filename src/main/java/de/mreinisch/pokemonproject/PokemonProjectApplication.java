26fd45f8a9c44fb241ba9cf2877aa68846cbc9fa
m-reinisch <michael.reinisch@email.de>
2026-06-02 10:01:31 +0200

    Initial commit

---
+++ src/main/java/de/mreinisch/pokemonproject/PokemonProjectApplication.java
@@ -0,0 +1,13 @@
+package de.mreinisch.pokemonproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokemonProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/mreinisch/pokemonproject/PokemonProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mreinisch.pokemonproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
